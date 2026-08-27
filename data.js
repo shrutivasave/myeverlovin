2dea6d2d5e66057df285183dccce0a520d0b2920
shrutivasave <shrutivasave@gmail.com>
2026-08-26 20:49:50 -0400

    added two flyers

--- data.js
+++ data.js
@@ -2,13 +2,6 @@
 
 const artistsData = [
   { 
-    id: 'name3', 
-    name: 'zachshots', 
-    image: 'assets/artist-zachshots.jpg', 
-    ig: '@zachshots2002', 
-    bio: 'zachshots is a two piece twee meets emo band consisting of zach galsky and annika haakonsen. their earnest approach to making music and intimate live shows have become a mainstay in brooklyn over the past couple of years. looking ahead, they are excited about dropping their debut record and playing more shows.' 
-  },
-  { 
     id: 'name1', 
     name: 'cash only tony\'s', 
     image: 'assets/artist-cash-only-tonys.jpeg', 
@@ -16,18 +9,18 @@ const artistsData = [
     bio: 'cash only tony\'s is a 7-piece screamo band from brooklyn new york. their spastic live shows and diy ethos have won the hearts of scenes across the country. they look forward to putting out new music and touring later on in 2026.'
   },
   { 
-    id: 'name5', 
-    name: 'dj funny pat', 
-    image: 'assets/artist-funny-pat.jpeg', 
-    ig: '@funny_pat_haha', 
-    bio: 'dj funny pat is a dj, is funny, and his name is pat. he plays shows, makes music, hosts a radio show, and is a true staple in the underground brooklyn music scene. he has new music coming out in 2026.' 
-  },
-  { 
     id: 'name2', 
     name: 'hooky', 
     image: 'assets/artist-hooky.jpeg', 
     ig: '@h0o0ky', 
     bio: 'hooky is a band from philadelphia, pennsylvania with two members, scott turner and sam silbert. they make music true to themselves and the world around them. they are frequent collaborators with local label julia’s war, on which they’ve dropped the majority of their music, including 2024 fan favorite mirage, and molly picture by god of war, the collaborative outlet of hooky and they are gutting a body of water. in 2026 they opened for alex g, toured with feeble little horse, and have enjoyed writing and recording new music. they fear no man. hooky also enjoys fishing and camping.'
+  },
+  { 
+    id: 'name3', 
+    name: 'zachshots', 
+    image: 'assets/artist-zachshots.jpg', 
+    ig: '@zachshots2002', 
+    bio: 'zachshots is a two piece twee meets emo band consisting of zach galsky and annika haakonsen. their earnest approach to making music and intimate live shows have become a mainstay in brooklyn over the past couple of years. looking ahead, they are excited about dropping their debut record and playing more shows.' 
   },
   { 
     id: 'name4', 
@@ -42,6 +35,13 @@ perfectly imperfect named it on their best of 2025 list, lauding, “when i put 
 
 emond also performs under the moniker alain c. montgomery, which has seen a co-sign from electronica legend dntel & the postal service’s jimmy tamborello and a forthcoming remix album via crane operator including reworks by ghost orchard, ideasforconversations, greg davis and josephine’s next million miles.`,
 },
+  { 
+    id: 'name5', 
+    name: 'dj funnypat', 
+    image: 'assets/artist-funny-pat.jpeg', 
+    ig: '@funny_pat_haha', 
+    bio: 'dj funny pat is a dj, is funny, and his name is pat. he plays shows, makes music, hosts a radio show, and is a true staple in the underground brooklyn music scene. he has new music coming out in 2026.' 
+  },
 ];
 
 const showsData = [
